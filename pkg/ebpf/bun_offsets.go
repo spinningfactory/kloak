2603cdb4ea6293318848a13702594151f2ac4410
Dhia Ayachi <dhia.ayachi@gmail.com>
2026-08-30 17:00:37 -0400

    chore(bun): track offsets for 1.4.0 (#296)

--- pkg/ebpf/bun_offsets.go
+++ pkg/ebpf/bun_offsets.go
@@ -37,6 +37,8 @@ var bunOffsetTable = map[string]BunOffsets{
 	"1.3.12/arm64": {SSLWriteOffset: 76482624, BoringSSL: BoringSSLOffsets{SSLToS3: 48, S3ToAEAD: 272, AEADToAESKey: 288, SSLToWBIO: 32}}, // discovered from bun-v1.3.12 profile build
 	"1.3.13/amd64": {SSLWriteOffset: 79149632, BoringSSL: BoringSSLOffsets{SSLToS3: 48, S3ToAEAD: 272, AEADToAESKey: 288, SSLToWBIO: 32}}, // discovered from bun-v1.3.13 profile build
 	"1.3.13/arm64": {SSLWriteOffset: 77478272, BoringSSL: BoringSSLOffsets{SSLToS3: 48, S3ToAEAD: 272, AEADToAESKey: 288, SSLToWBIO: 32}}, // discovered from bun-v1.3.13 profile build
+	"1.4.0/amd64":  {SSLWriteOffset: 35821600, BoringSSL: BoringSSLOffsets{SSLToS3: 48, S3ToAEAD: 272, AEADToAESKey: 288, SSLToWBIO: 32}}, // discovered from bun-v1.4.0 profile build
+	"1.4.0/arm64":  {SSLWriteOffset: 34985344, BoringSSL: BoringSSLOffsets{SSLToS3: 48, S3ToAEAD: 272, AEADToAESKey: 288, SSLToWBIO: 32}}, // discovered from bun-v1.4.0 profile build
 }
 
 var bunVersionRe = regexp.MustCompile(`bun/(\d+\.\d+\.\d+)`)
